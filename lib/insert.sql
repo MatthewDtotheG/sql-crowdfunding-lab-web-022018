12826644e2339a374531f96c3138c6a02bdd8c91
github username <github email address>
2018-02-27 14:20:50 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,74 @@
+INSERT INTO users (name, age) VALUES ("Bob", 27);
+INSERT INTO users (name, age) VALUES ("Matt", 27);
+INSERT INTO users (name, age) VALUES ("Zach", 27);
+INSERT INTO users (name, age) VALUES ("Tom", 27);
+INSERT INTO users (name, age)VALUES ("Bill", 27);
+INSERT INTO users (name, age) VALUES ("Joe", 27);
+INSERT INTO users (name, age) VALUES ("Zach", 27);
+INSERT INTO users (name, age) VALUES ("Matt", 27);
+INSERT INTO users (name, age) VALUES ("Matt", 27);
+INSERT INTO users (name, age) VALUES ("Matt", 27);
+INSERT INTO users (name, age) VALUES ("Matt", 27);
+INSERT INTO users (name, age) VALUES ("Matt", 27);
+INSERT INTO users (name, age) VALUES ("Matt", 27);
+INSERT INTO users (name, age) VALUES ("Matt", 27);
+INSERT INTO users (name, age) VALUES ("Matt", 27);
+INSERT INTO users (name, age) VALUES ("Matt", 27);
+INSERT INTO users (name, age) VALUES ("Matt", 27);
+INSERT INTO users (name, age) VALUES ("Matt", 27);
+INSERT INTO users (name, age) VALUES ("Matt", 27);
+INSERT INTO users (name, age) VALUES ("Matt", 27);
+
+-- PROJECTS
+
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Cool Title", "Cool category", 270, 02/12/18, 12/12/18 );
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Cool Title", "Cool category", 270, 02/12/18, 12/12/18 );
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Cool Title", "Cool category", 270, 02/12/18, 12/12/18 );
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Cool Title", "Cool category", 270, 02/12/18, 12/12/18 );
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Cool Title", "Cool category", 270, 02/12/18, 12/12/18 );
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Cool Title", "Cool category", 270, 02/12/18, 12/12/18 );
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Cool Title", "Cool category", 270, 02/12/18, 12/12/18 );
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Cool Title", "Cool category", 270, 02/12/18, 12/12/18 );
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Cool Title", "Cool category", 270, 02/12/18, 12/12/18 );
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Cool Title", "Cool category", 270, 02/12/18, 12/12/18 );
+
+-- PLEDGES
+
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+INSERT INTO pledges (amount, user_id, project_id) VALUES (1000, "Cool User", 1);
+
+
+
+-- CREATE TABLE pledges(
+-- id INTEGER PRIMARY KEY,
+-- amount INTEGER,
+-- user_id TEXT,
+-- project_id INTEGER);
